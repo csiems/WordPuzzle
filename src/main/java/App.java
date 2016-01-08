da0c6dc9eb0cb7154bc9e3816a9ac5341e7bfc35
origin <csiems@gmail.com>
2016-01-08 10:15:42 -0800

    add Spark page

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,40 +1,38 @@
-import java.io.Console;
+import java.util.Map;
 import java.util.HashMap;
-// import spark.ModelAndView;
-// import spark.template.velocity.VelocityTemplateEngine;
-import static spark.Spark.*;
 import java.util.Arrays;
+
+import spark.ModelAndView;
+import spark.template.velocity.VelocityTemplateEngine;
+import static spark.Spark.*;
+
 
 public class App{
   public static void main(String[] args){
-  //   staticFileLocation("/public");
-  //   String layout = "templates/layout.vtl";
-  //
-  //   get("/", (request, response) -> {
-  //     HashMap<String, Object> model = new HashMap();
-  //
-  //     model.put("template", "templates/form.vtl");
-  //     return new ModelAndView(model, layout);
-  //   }, new VelocityTemplateEngine());
-  //
-  //   get("/", (request, response) -> {
-  //     HashMap<String, Object> model = new HashMap();
-  //     //Declare your variables that will be used.
-  //     String user1Input = request.queryParams("user1Input");
-  //     String user2Input = computerChooses();
-  //     String winner = checkWinner(user1Input, user2Input);
-  //     //Put variables into HashMap; the first string is what is used in your forms
-  //     //with a $ in front of it; the second string is the variable it is matched to.
-  //     model.put("user1Input", user1Input);
-  //     model.put("user2Input", user2Input);
-  //     model.put("winner", winner);
-  //     model.put("template", "templates/output.vtl");
-  //
-  //     return new ModelAndView(model, layout);
-  //   }, new VelocityTemplateEngine());
+    staticFileLocation("/public");
+    String layout = "templates/layout.vtl";
+
+    get("/", (request, response) -> {
+      Map<String, Object> model = new HashMap<String, Object>();
+      model.put("template", "templates/form.vtl");
+      return new ModelAndView(model, layout);
+    }, new VelocityTemplateEngine());
+
+    get("/output", (request, response) -> {
+      Map<String, Object> model = new HashMap<String, Object>();
+      model.put("template", "templates/output.vtl");
+
+      String userInput = request.queryParams("userInput");
+      String dashedLine = replaceTheVowels(userInput);
+
+      model.put("userInput", userInput);
+      model.put("dashedLine", dashedLine);
+
+      return new ModelAndView(model, layout);
+    }, new VelocityTemplateEngine());
   }
 
-    public String replaceTheVowels(String userInput) {
+    public static String replaceTheVowels(String userInput) {
       String[] vowels = {"A", "E", "I", "O", "U", "a", "e", "i", "o", "u"};
       String dashedLine = "";
 
